023c030ac758c39c41649ee60290b73746f23136
Bianchi Valerio <v.bianchi@eustema.it>
2019-11-20 10:03:17 +0100

    mycommit2

--- src/app/youtube.service.ts
+++ src/app/youtube.service.ts
@@ -10,15 +10,16 @@ export class YoutubeService {
 
   current = new Subject<any>();
   currentVideo = {};
-  apiKey: string = 'AIzaSyDbby-7SWTFi9jZ9iF3uxyUwNJGZ369Ryg';
+  apiKey: string = 'AIzaSyA9vzbs_ZmYBNMgv7o9InzwBzZw5fyMQ3o';
   position: any = {};
 
   constructor(private http: HttpClient) { }
 
   getVideos():Observable<Object> {
     var date = new Date();
+    console.log(date.getMonth());
     return this.http.get(
-      'https://www.googleapis.com/youtube/v3/search?key='+this.apiKey+'&location=41.894802%2C12.4853384&locationRadius=10km&regionCode=IT&order=viewCount&part=snippet&type=video&maxResults=40&publishedAfter='+date.getFullYear().toString()+'-'+date.getMonth().toString()+'-'+(date.getDate()-1).toString()+'T'+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds()+'Z'
+      'https://www.googleapis.com/youtube/v3/search?key='+this.apiKey+'&location=41.894802%2C12.4853384&locationRadius=10km&regionCode=IT&order=viewCount&part=snippet&type=video&maxResults=40&publishedAfter='+date.getFullYear().toString()+'-'+(date.getMonth()+1).toString()+'-'+(date.getDate()-1).toString()+'T'+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds()+'Z'
       ).pipe(map(res=>{return res}));
   }
 
